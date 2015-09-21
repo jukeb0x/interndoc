1258a2f1c6731128d152b4f4338feb277b699d72
mathilde@kerdigo <mathleroch@gmail.com>
2015-09-21 18:25:00 +0200

    Test to create or modify XML files

--- src/launcher/Appli.java
+++ src/launcher/Appli.java
@@ -1,5 +1,12 @@
 package launcher;
+import utils.XMLParser;
 
 public class Appli {
+	 public static void main(String argv[]) {
+		 XMLParser parser=new XMLParser();
+		 //parser.XML_parse();
+		 parser.modifyMXL();
+	 }
+	 
 
 }
